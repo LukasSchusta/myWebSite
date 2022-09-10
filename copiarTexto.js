16f27d8aca436e03e40025a979d134860b800f34
Lukas Schuster <87291338+LukasSchusta@users.noreply.github.com>
2022-09-10 00:24:12 -0300

    modifiquei o negocio de copiar

--- copiarTexto.js
+++ copiarTexto.js
@@ -1,6 +1,8 @@
 function copiarTexto(){
+    textoCopiado = "29f5a83c-b3a8-49b1-a2d8-209b5b7d6ea0"
+    navigator.clipboard.writeText(textoCopiado)
+    textoCopiado.select();
+    textoCopiado.setSelectionRange(0, 99999)
+    document.execCommand("copy");
     
-    navigator.clipboard.writeText("29f5a83c-b3a8-49b1-a2d8-209b5b7d6ea0")
-    
-    alert("Copiado")
 }
