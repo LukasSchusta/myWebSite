d7ccf6303a44b98c24f6e9e921e734bc4e3f2f8b
Lucas Schuster <87291338+LukasSchusta@users.noreply.github.com>
2023-07-20 17:19:25 -0300

    adicionado julius

--- script/copiarTexto.js
+++ script/copiarTexto.js
@@ -2,9 +2,6 @@ function copiarTexto(){
     textoCopiado = "29f5a83c-b3a8-49b1-a2d8-209b5b7d6ea0"
     document.getElementById("copiado").style.visibility = "visible"
     navigator.clipboard.writeText(textoCopiado)
-    textoCopiado.select();
-    textoCopiado.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-    
+    document.getElementById("qr-code-pix").src="../img/julius-rock.jpg";
     
 }
