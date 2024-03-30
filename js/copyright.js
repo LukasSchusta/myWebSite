253c504749d10f4f82acf69ef683c00ca3676b23
Lucas Schuster <87291338+LukasSchusta@users.noreply.github.com>
2024-03-30 03:23:13 -0300

    Update copyright.js

--- js/copyright.js
+++ js/copyright.js
@@ -1,4 +1,4 @@
 window.onload = function copyright() {
   year = new Date().getFullYear() ;
-  document.getElementById('copyright').innerText = 'COPYRIGHT © ' + year + ', Lucas Eduaro Schuster.'
+  document.getElementById('copyright').innerText = 'COPYRIGHT © ' + year + ', Lucas Eduardo Schuster.'
 }
